0ec6a7daec2cb4658f6830b91c78c110728db78b
Ali Nauman <ali60351@gmail.com>
2017-12-28 14:08:12 +0500

    Added animation plus select tab

--- js/script.js
+++ js/script.js
@@ -1,14 +1,15 @@
 var app = new Vue({
     el: '#app',
     data: {
+        selectingP: false,
+        loading: false,
         homeP: true,
         connectP: false,
         bindingsP: false,
         securityP: false,
         pcCount: 2,
         sslState: 'disabled',
-        selectList: [
-            {
+        selectList: [{
                 name: 'Dell 123',
                 ip: '192.168.1.10'
             },
@@ -17,8 +18,7 @@ var app = new Vue({
                 ip: '192.168.1.11'
             }
         ],
-        bindingList: [
-            {
+        bindingList: [{
                 action: 'Copy',
                 binding: 'Ctrl + C'
             },
@@ -26,22 +26,34 @@ var app = new Vue({
                 action: 'Paste',
                 binding: 'Ctrl + V'
             }
+        ],
+        connectedList: [{
+                name: 'Dell 123',
+                ip: '192.168.1.10'
+            },
+            {
+                name: 'HP 321',
+                ip: '192.168.1.11'
+            }
         ]
     },
     methods: {
         homeClick: function () {
             this.homeP = true;
             this.connectP = false;
+            this.selectingP = false;
             this.bindingsP = false;
             this.securityP = false;
         },
         connectClick: function () {
             this.homeP = false;
+            this.selectingP = false;
             this.connectP = true;
             this.bindingsP = false;
             this.securityP = false;
         },
         bindingsClick: function () {
+            this.selectingP = false;
             this.homeP = false;
             this.connectP = false;
             this.bindingsP = true;
@@ -52,6 +64,7 @@ var app = new Vue({
             this.connectP = false;
             this.bindingsP = false;
             this.securityP = true;
+            this.selectingP = false;
         },
         toggleSSL: function () {
             if (this.sslState == 'disabled') {
@@ -59,6 +72,10 @@ var app = new Vue({
             } else {
                 this.sslState = 'disabled';
             }
+        },
+        searchDevices: function () {
+            this.connectP = false;
+            this.selectingP = true;
         }
     }
 });
